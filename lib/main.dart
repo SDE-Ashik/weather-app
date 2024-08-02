a7a688e2caefbbde4eb0e2b279f5e2c66398e9ae
ashik <ashiashik5048@gmail.com>
2024-08-03 03:55:23 +0530

    changed launcher icon and app name

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:provider/provider.dart';
 import 'package:weatherapp/screens/home_page.dart';
 import 'package:weatherapp/services/location_provider.dart';
 import 'package:weatherapp/services/weatherservice_provider.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    initialization();
+  }
+
+  void initialization() async {
+    print("pausing");
+    await Future.delayed(const Duration(seconds: 3));
+    print("unpausing");
+    FlutterNativeSplash.remove();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (context) => LocationProvider(),),
-        ChangeNotifierProvider(create: (context)=>WeatherserviceProvider()),
+        ChangeNotifierProvider(
+          create: (context) => LocationProvider(),
+        ),
+        ChangeNotifierProvider(create: (context) => WeatherserviceProvider()),
       ],
       child: MaterialApp(
         title: 'Weather app',
         theme: ThemeData(
           appBarTheme: const AppBarTheme(
-            backgroundColor:Colors.transparent,
-            
+            backgroundColor: Colors.transparent,
             elevation: 0,
           ),
-          
-         iconTheme:const IconThemeData(
-          color: Colors.white
-          
-         ),
-         
+          iconTheme: const IconThemeData(color: Colors.white),
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
@@ -42,6 +60,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-
- 
